d37f3fa217a0e0c4f24f38cf253c484ecf5ed1fd
Roland Onofrej <onofrejr@gmail.com>
2015-10-22 23:32:15 +0200

    First commit

---
+++ testOnky3/A/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: student
+ *
+ * Created on February 11, 2013, 7:30 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+    
+    return (EXIT_SUCCESS);
+}
+
